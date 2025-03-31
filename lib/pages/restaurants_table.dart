de38594716bbe6b8789db32b84956e1fdb57cd59
Ricky Magno <106983140+Ilyicky@users.noreply.github.com>
2025-03-31 11:41:24 +0800

    Update restaurants_table.dart

--- lib/pages/restaurants_table.dart
+++ lib/pages/restaurants_table.dart
@@ -12,11 +12,6 @@ class RestaurantsTable extends StatefulWidget {
 }
 
 class _RestaurantsTableState extends State<RestaurantsTable> {
-  /// Add handlers to buttons later on
-  /// For call and maps we can use url_launcher package.
-  /// We can also create a turn-by-turn navigation for a particular restaurant.
-  /// 🔥 Let's look at it in the next video!!
-
   Widget cardButtons(IconData iconData, String label) {
     return Padding(
       padding: const EdgeInsets.only(right: 10),
@@ -44,90 +39,90 @@ class _RestaurantsTableState extends State<RestaurantsTable> {
         title: const Text('Restaurants Table'),
       ),
       body: SafeArea(
-          child: SingleChildScrollView(
-        physics: const ScrollPhysics(),
-        child: Padding(
-          padding: const EdgeInsets.all(15),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const CupertinoTextField(
-                prefix: Padding(
-                  padding: EdgeInsets.only(left: 15),
-                  child: Icon(Icons.search),
+        child: SingleChildScrollView(
+          physics: const ScrollPhysics(),
+          child: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const CupertinoTextField(
+                  prefix: Padding(
+                    padding: EdgeInsets.only(left: 15),
+                    child: Icon(Icons.search),
+                  ),
+                  padding: EdgeInsets.all(15),
+                  placeholder: 'Search dish or restaurant name',
+                  style: TextStyle(color: Colors.white),
+                  decoration: BoxDecoration(
+                    color: Colors.black54,
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                  ),
                 ),
-                padding: EdgeInsets.all(15),
-                placeholder: 'Search dish or restaurant name',
-                style: TextStyle(color: Colors.white),
-                decoration: BoxDecoration(
-                  color: Colors.black54,
-                  borderRadius: BorderRadius.all(Radius.circular(5)),
-                ),
-              ),
-              const SizedBox(height: 5),
-              ListView.builder(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                padding: EdgeInsets.zero,
-                itemCount: restaurants.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Card(
-                    clipBehavior: Clip.antiAlias,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15)),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CachedNetworkImage(
-                          height: 175,
-                          width: 140,
-                          fit: BoxFit.cover,
-                          imageUrl: restaurants[index]['image'],
-                        ),
-                        Expanded(
-                          child: Container(
+                const SizedBox(height: 5),
+                ListView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  padding: EdgeInsets.zero,
+                  itemCount: restaurants.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CachedNetworkImage(
                             height: 175,
-                            padding: const EdgeInsets.all(15),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  restaurants[index]['name'],
-                                  style: const TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 16),
-                                ),
-                                Text(restaurants[index]['items']),
-                                const Spacer(),
-                                const Text('Waiting time: 2hrs'),
-                                Text(
-                                  'Closes at 10PM',
-                                  style:
-                                      TextStyle(color: Colors.redAccent[100]),
-                                ),
-                                Row(
-                                  children: [
-                                    cardButtons(Icons.call, 'Call'),
-                                    cardButtons(Icons.location_on, 'Map'),
-                                    const Spacer(),
-                                    Text(
-                                      '${(getDistanceFromSharedPrefs(index)/1000).toStringAsFixed(2)}km'),
-                                  ],
-                                )
-                              ],
-                            ),
+                            width: 140,
+                            fit: BoxFit.cover,
+                            imageUrl: restaurants[index]['image'],
                           ),
-                        )
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ],
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(15),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    restaurants[index]['name'],
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16),
+                                  ),
+                                  Text(restaurants[index]['items']),
+                                  const SizedBox(height: 8),
+                                  const Text('Waiting time: 2hrs'),
+                                  Text(
+                                    'Closes at 10PM',
+                                    style:
+                                    TextStyle(color: Colors.redAccent[100]),
+                                  ),
+                                  Row(
+                                    children: [
+                                      cardButtons(Icons.call, 'Call'),
+                                      cardButtons(Icons.location_on, 'Map'),
+                                      const Spacer(),
+                                      Text(
+                                          '${(getDistanceFromSharedPrefs(index) / 1000).toStringAsFixed(2)}km'),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
-      )),
+      ),
     );
   }
 }
